0890146c64863419e26060d6f9515bb902a03d1f
Marcos Coelho <mviniciusca@gmail.com>
2024-02-24 11:40:45 -0300

    wip

--- app/Http/Controllers/CepController.php
+++ app/Http/Controllers/CepController.php
@@ -3,29 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redis;
 
 class CepController extends Controller
 {
     public function show($cep)
     {
+
         $cep = preg_replace('/[^0-9]/', '', $cep);
 
-        if (strlen($cep) != 8 || !is_numeric($cep)) {
+        if (strlen($cep) !== 8) {
             return response()->json([
-                'message' => 'CEP invalid. Check the CEP and try again.'
+                'message' => 'Invalid CEP. The CEP must have 8 digits.'
             ], 400);
         }
 
-        // try to reach api
+        $cachedAddress = Redis::get($cep);
+        if ($cachedAddress) {
+            return response()->json(['data' => json_decode($cachedAddress)]);
+        }
         try {
             $response = file_get_contents(env('VIA_CEP_URL') . $cep . '/' . env('VIA_CEP_FORMAT'));
             $address = json_decode($response);
+
             if (isset($address->erro)) {
                 return response()->json([
                     'message' => 'CEP not found. Check the CEP and try again.'
                 ], 404);
             }
+
+            Redis::setex($cep, 3600, $response);
+
             return response()->json(['data' => $address]);
         } catch (\Exception $e) {
             return response()->json([
