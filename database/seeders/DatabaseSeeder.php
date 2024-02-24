04ea02d15adb8f46af0b630bb99058d5c88214c9
Marcos Coelho <mviniciusca@gmail.com>
2024-02-24 09:08:00 -0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Patient::factory()
-            ->count(10)
+            ->count(0)
             ->create();
     }
 }
