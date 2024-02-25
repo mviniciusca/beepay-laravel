1a27d53e7386c59ed1a675845495128e8e35681d
Marcos Coelho <mviniciusca@gmail.com>
2024-02-25 10:46:21 -0300

    wip

--- app/Http/Controllers/ImportPatientController.php
+++ app/Http/Controllers/ImportPatientController.php
@@ -16,7 +16,7 @@ class ImportPatientController extends Controller
         }
 
         $file = Validator::make($request->all(), [
-            'file' => 'required|file|mimes:csv,txt'
+            'file' => ['required', 'file', 'mimes:csv,txt']
         ]);
 
         if ($file->fails()) {
@@ -25,32 +25,84 @@ class ImportPatientController extends Controller
 
         $file = $file->validated()['file'];
 
-        $fileContents = file($file->getPathname());
+        $fileContents = $this->csvHandle($file);
 
-        foreach ($fileContents as $line) {
-            $data = str_getcsv($line);
-            if (count($data) >= 13) {
-                $patient = Patient::create([
-                    'full_name' => $data[0],
-                    'mother_name' => $data[1],
-                    'birth_date' => $data[2],
-                    'cpf' => $data[3],
-                    'cns' => $data[4],
-                    'picture' => $data[5],
-                ]);
-                Address::create([
-                    'zip_code' => $data[6],
-                    'street' => $data[7],
-                    'number' => $data[8],
-                    'complement' => $data[9],
-                    'district' => $data[10],
-                    'city' => $data[11],
-                    'state' => $data[12],
-                    'patient_id' => $patient->id,
-                ]);
+        if (empty($fileContents)) {
+            return response()->json(['message' => 'File is empty'], 400);
+        }
+
+        foreach ($fileContents as $data) {
+
+            $data['cpf'] = preg_replace('/[^0-9]/', '', $data['cpf']);
+            $data['cns'] = preg_replace('/[^0-9]/', '', $data['cns']);
+
+            $data_validation = Validator::make($data, [
+                'full_name' => ['required', 'string', 'max:255'],
+                'mother_name' => ['required', 'string', 'max:255'],
+                'birth_date' => ['required', 'date'],
+                'cpf' => ['required', 'string', 'max:11', 'unique:patients,cpf'],
+                'cns' => ['required', 'string', 'max:15', 'unique:patients,cns'],
+                'picture' => ['required', 'string', 'max:255'],
+                'zip_code' => ['required', 'string', 'max:10'],
+                'street' => ['required', 'string', 'max:255'],
+                'number' => ['required', 'string', 'max:10'],
+                'complement' => ['required', 'string', 'max:255'],
+                'district' => ['required', 'string', 'max:255'],
+                'city' => ['required', 'string', 'max:255'],
+                'state' => ['required', 'string', 'max:2'],
+            ]);
+
+            if ($data_validation->fails()) {
+                return response()->json([
+                    'message' => 'Error on validation. Please, check your data.',
+                    'errors' => [
+                        'fail' => $data_validation->errors(),
+                        'info' => $data,
+                    ]
+                ], 400);
             }
+
+            $patient = Patient::create([
+                'full_name' => $data['full_name'],
+                'mother_name' => $data['mother_name'],
+                'birth_date' => $data['birth_date'],
+                'cpf' => $data['cpf'],
+                'cns' => $data['cns'],
+                'picture' => $data['picture'],
+            ]);
+            Address::create([
+                'zip_code' => $data['zip_code'],
+                'street' => $data['street'],
+                'number' => $data['number'],
+                'complement' => $data['complement'],
+                'district' => $data['district'],
+                'city' => $data['city'],
+                'state' => $data['state'],
+                'patient_id' => $patient->id,
+            ]);
         }
         return response()->json(['message' => 'File imported successfully'], 200);
+    }
+
+    /**
+     * Summary of csvHandle
+     * This function will handle the csv file
+     * Checks if has header and if has, create a array combine with the header and the data
+     */
+    public function csvHandle($file)
+    {
+        $file = file($file->getPathname());
+        $header = null;
+        $data = [];
+        foreach ($file as $line) {
+            $line = str_getcsv($line);
+            if (!$header) {
+                $header = $line;
+            } else {
+                $data[] = array_combine($header, $line);
+            }
+        }
+        return $data;
     }
 }
 
